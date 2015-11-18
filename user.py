bd0652e5069856baf79c18552bd6dca41cfb46d1
svetoslavovm <svetoslavov.martin@gmail.com>
2015-11-18 08:40:05 -0500

    adds storyline to html rendering

--- user.py
+++ user.py
@@ -4,7 +4,7 @@ import fresh_tomatoes
 # Create 'Toy Story' movie instance
 toy_story = media.Movie(
     'Toy Story',
-    'A story about a bunch of toys that come alive',
+    'A story about a bunch of toys that come alive.',
     ('http://media.comicbook.com/uploads1/2015/03/' +
         'group-disney-announces-toy-story-4-is-happening-126226.jpeg'),
     'https://www.youtube.com/watch?v=KYz2wyBy3kc')
@@ -12,7 +12,7 @@ toy_story = media.Movie(
 # Create 'School of Rock' movie instance
 school_of_rock = media.Movie(
     'School of Rock',
-    'We don\'t need education',
+    'We don\'t need education, we need music!',
     ('http://ia.media-imdb.com/images/M/MV5BMjEwOTMzNjYzMl5BMl5BanBnXk' +
         'FtZTcwNjczMTQyMQ@@._V1_SX640_SY720_.jpg'),
     'https://www.youtube.com/watch?v=5afGGGsxvEA')
@@ -20,7 +20,7 @@ school_of_rock = media.Movie(
 # Create 'Ratatouille' movie instance
 ratatouille = media.Movie(
     'Ratatouille',
-    'He\'s dying to become a chef',
+    'A mouse dying to become a chef!',
     ('http://ia.media-imdb.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXk' +
         'FtZTcwMjQ4MzMzMw@@._V1_SX640_SY720_.jpg'),
     'https://www.youtube.com/watch?v=R1tcS0pBgY8')
