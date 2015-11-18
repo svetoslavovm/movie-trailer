a58715cda3e45bda2edf9d0834e75f957d501a52
svetoslavovm <svetoslavov.martin@gmail.com>
2015-11-18 01:00:07 -0500

    adds comments and syntax changes per PEP8

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+
 class Movie():
     """ A class that store basic movie information """
 
@@ -9,6 +10,7 @@ class Movie():
     def __main__():
         print(' __main__ RUNS !')
 
+    # Initialize class instance with appropriate values
     def __init__(self, title, storyline, image_url, youtube_url):
 
         self.title = title
@@ -16,18 +18,18 @@ class Movie():
         self.poster_image_url = image_url
         self.trailer_youtube_url = youtube_url
 
-
+    # Opens default browser and loads movie url
     def show_trailer(self):
-        webbrowser.open( self.trailer_youtube_url )
+        webbrowser.open(self.trailer_youtube_url)
 
-
+    # Returns movile title
     def get_title(self):
         if __name__ == '__main__':
             # execute only if run as a script
             main()
             __main__()
 
-            #__module__  <- what would this do ???
+            # __module__  <- what would this do ???
 
-        #print(self.title)
+        # print(self.title)
         return self.title
--- user.py
+++ user.py
@@ -1,30 +1,33 @@
 import media
 import fresh_tomatoes
 
+# Create 'Toy Story' movie instance
 toy_story = media.Movie(
     'Toy Story',
     'A story about a bunch of toys that come alive',
-    'http://media.comicbook.com/uploads1/2015/03/group-disney-announces-toy-story-4-is-happening-126226.jpeg',
+    ('http://media.comicbook.com/uploads1/2015/03/' +
+        'group-disney-announces-toy-story-4-is-happening-126226.jpeg'),
     'https://www.youtube.com/watch?v=KYz2wyBy3kc')
 
-
+# Create 'School of Rock' movie instance
 school_of_rock = media.Movie(
     'School of Rock',
     'We don\'t need education',
-    'http://ia.media-imdb.com/images/M/MV5BMjEwOTMzNjYzMl5BMl5BanBnXkFtZTcwNjczMTQyMQ@@._V1_SX640_SY720_.jpg',
+    ('http://ia.media-imdb.com/images/M/MV5BMjEwOTMzNjYzMl5BMl5BanBnXk' +
+        'FtZTcwNjczMTQyMQ@@._V1_SX640_SY720_.jpg'),
     'https://www.youtube.com/watch?v=5afGGGsxvEA')
 
+# Create 'Ratatouille' movie instance
 ratatouille = media.Movie(
     'Ratatouille',
     'He\'s dying to become a chef',
-    'http://ia.media-imdb.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_SX640_SY720_.jpg',
+    ('http://ia.media-imdb.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXk' +
+        'FtZTcwMjQ4MzMzMw@@._V1_SX640_SY720_.jpg'),
     'https://www.youtube.com/watch?v=R1tcS0pBgY8')
 
-#print( school_of_rock.storyline )
-
 movies = [toy_story, school_of_rock, ratatouille]
-fresh_tomatoes.open_movies_page( movies )
+fresh_tomatoes.open_movies_page(movies)
 
-#print ( media.Movie.__doc__ )
-#print ( media.Movie.__name__ + ' is in ' + media.Movie.__module__ + '.py' )
-#print ( toy_story.get_title() )
+# print ( media.Movie.__doc__ )
+# print ( media.Movie.__name__ + ' is in ' + media.Movie.__module__ + '.py' )
+# print ( toy_story.get_title() )
